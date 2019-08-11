442f664f9afe20ec51134ef74f66e12994e44adb
Filipe Kiss <hello@filipekiss.com.br>
2019-08-11 21:50:05 +0200

    Remove unused i18n

--- src/utils/i18n.js
+++
@@ -1,5 +0,0 @@
-import {supportedLanguages} from '../../i18n';
-
-export const defaultLanguage = 'en';
-export const languageNameFromCode = (code) =>
-    supportedLanguages[code].replace(/ /g, ' ');
