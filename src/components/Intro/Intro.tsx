10db8ef11c66634c3fdf1dc5c72af60e13a76d98
Filipe Kiss <hello@filipekiss.com.br>
2019-08-13 18:07:17 +0200

    Change style for intro component

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Intro = () => {
     return (
         <div className="mb-20 max-w-lg">
-            <div className="text-6xl">Hello, I'm Kiss,</div>
+            <div className="text-6xl text-victoria-500">Hello, I'm Kiss,</div>
             <p className="text-xl">
                 a developer based in Amsterdam, Netherlands. This blog is where
                 I'm going to write about development in general, typescript, zsh
